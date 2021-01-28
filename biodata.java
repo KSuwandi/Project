e525816514acc6303611faabcfa6261c0e2815d6
KSuwandi <77820188+KSuwandi@users.noreply.github.com>
2021-01-28 15:36:50 +0700

    Update biodata.java

--- biodata.java
+++ biodata.java
@@ -12,9 +12,9 @@ public class Biodata {
       
         
       
-      System.out.println("Nama = Acika Difa");
-      System.out.println("Nim = 1920013212");
-        System.out.println("Kelas = 3SC1");
+      System.out.println("Nama = Kevin Suwandi");
+      System.out.println("Nim = 1920010112");
+        System.out.println("Kelas = 3SC2");
         System.out.println("Jurusan = Software Enginering ");
         System.out.println("\n");
         System.out.println("--- Print Biodata seminar----");
